ea8024b726d7e4a8f5983a7a0e2d11ca6e3e028c
Sagar123412 <sainsagar96@gmail.com>
2024-06-26 13:44:34 +0530

    formated

--- src/services/TokenService.tsx
+++ src/services/TokenService.tsx
@@ -4,35 +4,35 @@ import { JwtPayload, sign } from 'jsonwebtoken';
 import path from 'path';
 import { Config } from '../config';
 
-export class TokenService{
-    generateAccessToken(payload: JwtPayload){
-         //private key for RS256
-      let privateKey: Buffer;
-      try {
-        privateKey = fs.readFileSync(
-          path.join(__dirname, '../../certs/private.pem'),
-        );
-      } catch (err) {
-        const error = createHttpError(500, 'Error while reading private key');
-        throw error;
-        return;
-      }
-
-      const accessToken = sign(payload, privateKey, {
-        algorithm: 'RS256',
-        expiresIn: '1h',
-        issuer: 'auth-service',
-      });
-
-      return accessToken;
+export class TokenService {
+  generateAccessToken(payload: JwtPayload) {
+    //private key for RS256
+    let privateKey: Buffer;
+    try {
+      privateKey = fs.readFileSync(
+        path.join(__dirname, '../../certs/private.pem'),
+      );
+    } catch (err) {
+      const error = createHttpError(500, 'Error while reading private key');
+      throw error;
+      return;
     }
-    generateRefreshToken(payload: JwtPayload){
-        const refreshToken = sign(payload, Config.REFRESH_TOKEN_SECRET!, {
-            algorithm: 'HS256',
-            expiresIn: '1y',
-            issuer: 'auth-service',
-            jwtid: String(payload.id),
-        });
-        return refreshToken;
-    }
+
+    const accessToken = sign(payload, privateKey, {
+      algorithm: 'RS256',
+      expiresIn: '1h',
+      issuer: 'auth-service',
+    });
+
+    return accessToken;
+  }
+  generateRefreshToken(payload: JwtPayload) {
+    const refreshToken = sign(payload, Config.REFRESH_TOKEN_SECRET!, {
+      algorithm: 'HS256',
+      expiresIn: '1y',
+      issuer: 'auth-service',
+      jwtid: String(payload.id),
+    });
+    return refreshToken;
+  }
 }
